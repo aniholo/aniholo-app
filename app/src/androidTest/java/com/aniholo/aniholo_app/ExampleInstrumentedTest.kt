5bfd0d37606cbdcd035480c7dcf9e2df662e3946
iterus <iterus@protonmail.com>
2019-02-13 18:15:18 +0300

    Converted sources to kotlin

---
+++ app/src/androidTest/java/com/aniholo/aniholo_app/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.aniholo.aniholo_app
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see [Testing documentation](http://d.android.com/tools/testing)
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+
+    @Test
+    fun testPackageName() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+
+        assertEquals("com.aniholo.aniholo_app", appContext.packageName)
+    }
+}
